d1c1cf490963d857eeacb7f5a126e74211ee518f
ligopsychakis-michalis <ligopsyxakis.m@gmail.com>
2020-05-06 18:45:51 +0300

    week2 react

---
+++ Week2/hackyourweather2/build/precache-manifest.6c539370ab169b0340175952400e1af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462607b25616fb8496b2443437bd3844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6ab71913bcfe81df5f9",
+    "url": "/static/css/main.471af7bf.chunk.css"
+  },
+  {
+    "revision": "1192a7eba899870433fb",
+    "url": "/static/js/2.3f1e1ecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f1e1ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ab71913bcfe81df5f9",
+    "url": "/static/js/main.00f6f358.chunk.js"
+  },
+  {
+    "revision": "eeaa1aa412cbaaa6f655",
+    "url": "/static/js/runtime-main.114fb931.js"
+  }
+]);
