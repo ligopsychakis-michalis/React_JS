7554450250cac83ad302b5674971c338ed9779e4
ligopsychakis-michalis <ligopsyxakis.m@gmail.com>
2020-05-11 09:53:20 +0300

    build folder

---
+++ Week3/recipes-app/build/precache-manifest.b7ec47f2215a657fec480859c2925bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f82a6d3f7c397c292c5b6f24a38777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2856fc5ff9ee12b69632",
+    "url": "/static/css/main.2813f3c4.chunk.css"
+  },
+  {
+    "revision": "8967c2d9bad9cdd8ef68",
+    "url": "/static/js/2.630595c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.630595c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2856fc5ff9ee12b69632",
+    "url": "/static/js/main.8ce3cc7f.chunk.js"
+  },
+  {
+    "revision": "f684bb7c844966a9b547",
+    "url": "/static/js/runtime-main.a1506010.js"
+  }
+]);
